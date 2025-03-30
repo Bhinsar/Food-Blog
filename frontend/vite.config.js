60bafea2c4d1b241c0569aa888082aee6b6f5081
Bhinsar Jagdish <125593539+Bhinsar@users.noreply.github.com>
2025-03-30 11:08:23 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+   server: {
+    port: 3000, 
+   },
   plugins: [react()],
   ss: {
     postcss: './postcss.config.js', // Ensure PostCSS is referenced here
